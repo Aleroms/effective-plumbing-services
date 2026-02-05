9508607a24ada2b62ea051918ed352f4f60a8dc7
Santiago <santiagomorales333@yahoo.com>
2026-02-04 21:02:25 -0800

    configured header and seo

--- app/app.vue
+++ app/app.vue
@@ -1,51 +1,78 @@
-<script setup>
+<script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui'
+
 useHead({
-  meta: [
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
-  ],
-  link: [
-    { rel: 'icon', href: '/favicon.ico' }
-  ],
+  meta: [{ name: 'viewport', content: 'width=device-width, initial-scale=1' }],
+  link: [{ rel: 'icon', href: '/favicon.ico' }],
   htmlAttrs: {
     lang: 'en'
   }
 })
 
-const title = 'Nuxt Starter Template'
-const description = 'A production-ready starter template powered by Nuxt UI. Build beautiful, accessible, and performant applications in minutes, not hours.'
+const title = 'Effective Plumbing Services'
+const title2 = title + ' | South Orange County Plumber (C-36 Licensed)'
+const description
+  = 'Licensed C-36 plumbing contractor in South Orange County offering residential and emergency plumbing, drain cleaning, water heater & sewer services.'
+
+const items = computed<NavigationMenuItem[]>(() => [
+  {
+    label: 'Services',
+    to: '/services',
+    icon: 'i-lucide-wrench'
+  },
+  {
+    label: 'About',
+    to: '/about',
+    icon: 'i-lucide-users'
+  },
+  {
+    label: 'Gallery',
+    to: '/gallery',
+    icon: 'i-lucide-gallery-vertical-end'
+  },
+  {
+    label: 'Contact',
+    to: '/contact',
+    icon: 'i-lucide-phone'
+  }
+])
 
 useSeoMeta({
-  title,
-  description,
-  ogTitle: title,
-  ogDescription: description,
-  ogImage: 'https://ui.nuxt.com/assets/templates/nuxt/starter-light.png',
-  twitterImage: 'https://ui.nuxt.com/assets/templates/nuxt/starter-light.png',
-  twitterCard: 'summary_large_image'
+  title: title,
+  description:
+    description,
+  ogTitle: title2,
+  ogDescription:
+    description,
+  ogType: 'website',
+  twitterCard: 'summary_large_image',
+  twitterTitle: title2,
+  twitterImage: 'https://lh3.googleusercontent.com/p/AF1QipPl6NGfpF3Wt1Xrz_OCPCpdydMAXDsMT-5I06ec=s680-w680-h510-rw',
+  twitterDescription:
+    description,
+  ogUrl: 'https://effectiveplumbingservices.com',
+  ogImage: 'https://lh3.googleusercontent.com/p/AF1QipPl6NGfpF3Wt1Xrz_OCPCpdydMAXDsMT-5I06ec=s680-w680-h510-rw'
 })
 </script>
 
 <template>
   <UApp>
-    <UHeader>
-      <template #left>
-        <NuxtLink to="/">
-          <AppLogo class="w-auto h-6 shrink-0" />
-        </NuxtLink>
-
-        <TemplateMenu />
+    <UHeader mode="slideover">
+      <template #title>
+        Effective Plumbing Services
       </template>
 
       <template #right>
-        <UColorModeButton />
+        <UNavigationMenu
+          :items="items"
+          class="hidden lg:flex"
+        />
+      </template>
 
-        <UButton
-          to="https://github.com/nuxt-ui-templates/starter"
-          target="_blank"
-          icon="i-simple-icons-github"
-          aria-label="GitHub"
-          color="neutral"
-          variant="ghost"
+      <template #body>
+        <UNavigationMenu
+          :items="items"
+          orientation="vertical"
         />
       </template>
     </UHeader>
